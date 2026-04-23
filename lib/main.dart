7c46357e5476163dd7c7c32145a39595b2df81e9
John Carl Arroza <95612131+johncarlarroza@users.noreply.github.com>
2026-04-23 15:40:41 +0800

    w/ debug / functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'firebase_options.dart';
 import 'theme/app_theme.dart';
 import 'providers/cart_provider.dart';
 import 'screens/welcome_screen.dart';
 
+Future<void> setupFunctionsEmulator() async {
+  if (kDebugMode) {
+    FirebaseFunctions.instanceFor(region: 'us-central1')
+        .useFunctionsEmulator('127.0.0.1', 5001);
+  }
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await setupFunctionsEmulator();
 
   runApp(const MyApp());
 }
@@ -20,7 +34,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => CartProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Vian Café POS',
